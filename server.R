80aa194dfa9e60ab39a6e0b75562d8a8b5fb6b27
Madan <kcma@mail.gvsu.edu>
2022-04-17 00:37:32 +0000

    published shiny

--- server.R
+++ server.R
@@ -1,15 +1,12 @@
+barchart_data <- read.csv("barchart_data.csv")
+counties <- read.csv("counties.csv")
+piechart_total <- read.csv("piechart_total.csv")
+crashes_total <- read.csv("crashes_total.csv")
+
 server <- function(input, output) {
-  barchart_data <- read.csv("barchart_data.csv")
-  
-  counties <- read.csv("counties.csv")
-  
-  piechart_total <- read.csv("piechart_total.csv")
-  
-  crashes_total <- read.csv("crashes_total.csv")
-  
   
   output$barplot <- renderPlot({
-    
+    library(reshape2)
     barchart_data <- barchart_data %>% filter(Type==input$Type)
     barchart_data <- melt(barchart_data[,c('year','Alcohol_total','Drug_total')], id.vars = 1)
     g <- ggplot(barchart_data, aes(x = year, y = value)) +
@@ -17,25 +14,14 @@ server <- function(input, output) {
     g
   })
   
-  output$barr <- renderPlot({
-    barchart_data <- barchart_data %>% filter(Type==input$type)
-    barchart1 <- data.frame(Year = barchart_data$year,
-                            Number1 = c(barchart_data$Alcohol_total, barchart_data$Drug_total),
-                            Type = c(rep("Alcohol Total", nrow(barchart_data)),
-                                     rep("Drug Total", nrow(barchart_data))))
-    g <- ggplot(barchart1, aes(Year, Number1, col = Type)) +
-      geom_bar()
-    g
-  })
-  
   #line chart of crash
   output$linechart1 <- renderPlot({
     cc <- counties
-    cc <- cc %>% filter(County==input$county)
-    county_crash <- data.frame(Year = cc$year,                            # Reshape data frame
-                               Number = c(cc$Alcohol_crash, cc$Drug_crash),
-                               Type = c(rep("Alcohol Crash", nrow(cc)),
-                                        rep("Drug Crash", nrow(cc))))
+    counties <- counties %>% filter(County==input$county)
+    county_crash <- data.frame(Year = counties$year,                            # Reshape data frame
+                               Number = c(counties$Alcohol_crash, counties$Drug_crash),
+                               Type = c(rep("Alcohol Crash", nrow(counties)),
+                                        rep("Drug Crash", nrow(counties))))
     
     ggp <- ggplot(county_crash, aes(Year, Number , col = Type)) +             # Create ggplot2 plot
       geom_line()
@@ -45,41 +31,16 @@ server <- function(input, output) {
   #Linechart of injuries and death
   output$linechart2 <- renderPlot({
     cc2 <- counties
-    cc2 <- cc2 %>% filter(County==input$county_injury)
-    injury_county <- data.frame(Years = cc2$year,                            # Reshape data frame
-                                Numbers = c(cc2$Alcohol_injury, cc2$Drug_injury),
-                                Types = c(rep("Alcohol Injuries and Death", nrow(cc2)),
-                                          rep("Drug Injuries and Death", nrow(cc2))))
+    counties <- counties %>% filter(County==input$county)
+    injury_county <- data.frame(Years = counties$year,                            # Reshape data frame
+                                Numbers = c(counties$Alcohol_injury, counties$Drug_injury),
+                                Types = c(rep("Alcohol Injuries and Death", nrow(counties)),
+                                          rep("Drug Injuries and Death", nrow(counties))))
     
     ggp <- ggplot(injury_county, aes(Years, Numbers , col = Types)) +             # Create ggplot2 plot
       geom_line()
     ggp 
   }) # end of Linechart of injuries and death
-  
-  # Start -- piechart of total crashes per year
-  output$piechart <- renderPlot({
-    piechart_total <- piechart_total %>% filter(year==input$Year)
-    df_crash <- data.frame(Types = c("alcohol", "drug", "others"),
-                           value=c(piechart_total$total_alcohol_crashes, piechart_total$total_drug_crashes, piechart_total$Other_Crashes)
-    )
-    ggp <-  ggplot(df_crash, aes(x="", y=value, fill=Types)) +
-      geom_bar(stat="identity", width=1) +
-      coord_polar("y", start=0) +
-      theme_void()
-    ggp
-  })
-  
-  output$piechart1 <- renderPlot({
-    piechart_total <- piechart_total %>% filter(year==input$Year)
-    df_crash <- data.frame(Types = c("alcohol", "drug", "others"),
-                           value=c(piechart_total$total_alcohol_crashes, piechart_total$total_drug_crashes, piechart_total$Other_Crashes)
-    )
-    ggp <-  ggplot(df_crash, aes(x="", y=value, fill=Types)) +
-      geom_bar(stat="identity", width=1) +
-      coord_polar("y", start=0) +
-      theme_void()
-    ggp
-  })
   
   
   output$piechart <- renderPlot({
@@ -93,7 +54,6 @@ server <- function(input, output) {
       theme_void()
     ggp
   })
-  
 }
 
 # Run the application 
--- ui.R
+++ ui.R
@@ -2,76 +2,59 @@
 
 library(shiny)
 library(shinythemes)
-library(ggplot2)
 library(dplyr)
-library(googlesheets4)
-library(tidyverse)
-library(plotly)
-library(ggmap)
-library(maps)
-library(mapproj)
-library(reshape2)
-library(leaflet)
+library(magrittr)
 library(sf)
-library(tigris)
+library(ggplot2)
+library(reshape2)
 
 barchart_data <- read.csv("barchart_data.csv")
-
 counties <- read.csv("counties.csv")
-
 piechart_total <- read.csv("piechart_total.csv")
-
 crashes_total <- read.csv("crashes_total.csv")
+
 
 title<-
   # Define UI
   ui <- fluidPage(theme = shinytheme("cerulean"),
                   navbarPage("Grand Rapids Crash",
-                             #theme = "cerulean",  # <--- To use a theme,
-                             tabPanel("Total Crash Comparison",
+                             tabPanel("Alcohol/Drug",
                                       sidebarPanel(
                                         selectInput("Type", "Select Type", choices = unique(barchart_data$Type))
-                                      ), # sidebarPanel
+                                        ), # sidebarPanel
                                       mainPanel(
-                                        plotOutput("barplot"),
-                                      ) # mainPanel
+                                       plotOutput("barplot"),
+                                       p("This is the line chart from the year 2004 to 2019. We can filter either Crash or injuries and death due to alcohol and drugs. Then, it compares the total crash dues to alcohol and drugs per each year."),
+                                       p("From the graph, we can visualize that the total number of crashes due alcohol is higher than that due to drugs."),
+                                       p("However, the injuries and death rate per crash is higher due to drugs than in alcohol though the number is higher.")
+                                     ) # mainPanel
                              ),
-                             tabPanel("Crashes - County", #countywise line chart of crash
+                             tabPanel("County", #countywise line chart of crash
                                       sidebarPanel(
                                         selectInput("county", "Select County", choices = unique(counties$County))
                                       ), # sidebarPanel
                                       mainPanel(
-                                        plotOutput("linechart1"),
+                                        tabsetPanel(
+                                          tabPanel("Crashes",plotOutput("linechart1")),
+                                          tabPanel("Injuries and Death",plotOutput("linechart2"))
+                                        ),
+                                        p("There are two tabs for the visualization for each counties: 1. Crashes 2. Injuries and death. The counties can be filtered from the sidebar panel."),
+                                        p("From the visualizations above, it is proven that the all the counties have higher number of crashes. However, number of injured and died people is higher in all of the counties."),
+                                        
+                                        
                                       ) # mainPanel
                              ), #end - countywise line chart of crash
                              
-                             tabPanel("Injuries and death - County", #countywise line chart of injury and death
-                                      sidebarPanel(
-                                        selectInput("county_injury", "Select One County", choices = unique(counties$County))
-                                      ), # sidebarPanel
-                                      mainPanel(
-                                        plotOutput("linechart2"),
-                                      ) # mainPanel
-                             ), #end - countywise line chart of injury and death
-                             
-                             tabPanel("Piechart1", #piechart
-                                      sidebarPanel(
-                                        selectInput("Year", "Select a Year", choices = unique(piechart_total$year))
-                                      ), # sidebarPanel
-                                      mainPanel(
-                                        plotOutput("piechart1"),
-                                      ) # mainPanel
-                             ), #end - countywise line chart of injury and death
-                             
-                             tabPanel("Total Crashes", #piechart
+                             tabPanel("Total Crashes - Year", #piechart
                                       sidebarPanel(
                                         selectInput("time", "Select a Year", choices = unique(piechart_total$year))
                                       ), # sidebarPanel
                                       mainPanel(
                                         plotOutput("piechart"),
+                                        p("We found that Kent has almost one-third crashes among four counties in Grand Rapids Metropolitan Area in each year from 2004 to 2019."),
+                                        p("Ottawa  is the second county having the higher number of crashes in that period of time. Besides that, Montcalm and Ionia has almost same range of crashes")
                                       ) # mainPanel
                              )
-                             
                   ) #nav bar 
   ) #ui 
 
